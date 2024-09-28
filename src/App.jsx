90875c2e18690d34aa69bca6efc400413d7bb3c4
karen13hb <karenbj15@hotmail.com>
2024-09-27 19:45:33 -0500

    host-mf

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import ColorPicker from "colorPicker/ColorPicker";
+import ColorList from "colorList/ColorList";
+
+import {useColors} from "colorPicker/useColors";
+
+const App = () => {
+
+  const { color, colorsList, handleChangeColor, handleSubmitSaveColor} = useColors();
+  return (<>
+    <h1 className="text-center bg-dark text-white p-2">Color picker</h1>
+    <div className="container mt-4"> 
+      <div className="row">
+        <div className="col-12 col-md-4">
+          <ColorList
+          colorsList = {colorsList}/>
+        </div>
+        <div className="col-12 col-md-8">
+          <ColorPicker
+          color ={color}
+          handleChangeColor={handleChangeColor}
+          handleSubmitSaveColor={handleSubmitSaveColor}
+          />
+        </div>
+      </div>
+    </div>
+  </>)
+};
+const rootElement = document.getElementById("app")
+if (!rootElement) throw new Error("Failed to find the root element")
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(<App />)
